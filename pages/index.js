758ab12490e187b179e9fe9c713ae1ac9bc31bba
Damien Snyder <damiensnyder@damiensnyder.com>
2020-09-29 19:03:46 -0700

    basic layout work

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,33 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Head from 'next/head';
+import styles from '../components/home/home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.pageContainer}>
       <Head>
-        <title>Create Next App</title>
+        <title>Damien Snyder</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <h1 className={styles.pageTitle}>
+        Damien Snyder
+      </h1>
+      <div className={styles.menusContainer}>
+        <div className={styles.linksMenu}>
+          <h2 className={styles.menuTitle}>Recent content</h2>
+          <div className={styles.menuItem}>
+            <img className={styles.thumbnail}
+                src={"https://damiensnyder.com/thumbs/it-isnt-funny-anymore.png"} />
+            <div className={styles.itemText}>
+              <span className={styles.itemTitle}>it isn't funny anymore</span>
+              <span className={styles.itemDate}>2020.05.28</span>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className={styles.linksMenu}>
+          <h2 className={styles.menuTitle}>Other pages</h2>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
