78156ea18445ce631531ee616b93ffcdecd6f015
Damien Snyder <damiensnyder@damiensnyder.com>
2020-10-25 23:33:23 -0700

    change blog to use things other people made
    NOT rolling my own, why would i do thaitahaha

--- content/blog/this-is-my-first-blog-post.md
+++ content/blog/this-is-my-first-blog-post.md
@@ -1,38 +1,10 @@
-that's right, motherfuckers. the very first one
+# section 1: how to fit garbage in your trash can
 
-i shit out my asshole and so should you
+- ~~step 1: refer to step 2~~[^1]
+- step 2: open the trash can
+- step 3: there might already be garbage in there. that's ok
+- step 4: gbaebrge in thei ntrash c=an
 
-watch this, watch, im gonna do some fancy code
+and you've done it!
 
-# secion 1 : hedings are 4 hores
-
-```
-hello world? more like
-      hello IDIOT
-```
-
-and im `inline` baby
-
-test for *italics* and **bold**, ~~or whatever i decide to do instead~~
-
-a [link](https://tagpro.gg) and a footnote!^^my [footnote](piss baby) here^^ or
-sidenote or however it turns out
-
-![an image, and this is the alt text](/thumbs/twitter.jpg)
-
-caption: default caption
->>>
-like a
-
-* bullet
-* point
-* list
->>>
-maybe in the future, math
-
-## heding 2 has *italics* b/c
-
-this line gets kind of **fuck**ed up
-and i don't know why. oh geeeeeee willikers
-
-oh g
+[^1]: thing has been deprecated
---
+++ pages/blog/[code].tsx
@@ -0,0 +1,50 @@
+import React, {ReactElement} from "react";
+import general from "../../styles/general.module.css";
+import styles from "../../styles/blog.module.css";
+import {BlogPostProps, getBlogPost} from "../api/blog/[post]";
+import {Paths, getPostPaths} from "../api/content";
+import LinkHeader from "../../components/LinkHeader";
+import NormalHead from "../../components/NormalHead";
+import {formatDate} from "../../components/MenuItem";
+
+// Markdown parsing
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
+import {InlineMath, BlockMath} from "react-katex";
+import math from "remark-math";
+import "katex/dist/katex.min.css";
+
+const renderers = {
+  inlineMath: ({value}) => <InlineMath math={value} />,
+  math: ({value}) => <BlockMath math={value} />
+}
+
+export default function BlogPostPage(props: BlogPostProps): ReactElement {
+  return (
+    <div className={general.pageContainer}>
+      <NormalHead title={props.name} />
+      <div className={general.postContainer}>
+        <LinkHeader path={["blog"]} />
+        <h1 className={general.pageTitle}>
+          {props.name}
+        </h1>
+        <p className={general.caption}>
+          posted {formatDate(props.date)}
+        </p>
+        <ReactMarkdown renderers={renderers}
+                       plugins={[math, [gfm, {singleTilde: false}]]}
+                       source={props.text} />
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps(context): Promise<{props: BlogPostProps}> {
+  return {
+    props: await getBlogPost(context.params.code)
+  };
+}
+
+export async function getStaticPaths(): Promise<Paths> {
+  return await getPostPaths("blog");
+}
