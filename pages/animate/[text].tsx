c68a34b445a4664c6e0f3794ca6d4d1ab324bd2c
Damien Snyder <damiensnyder@damiensnyder.com>
2021-02-24 01:27:31 -0800

    fix animation i hope

--- pages/animate/[text].tsx
+++ pages/animate/[text].tsx
@@ -4,25 +4,27 @@ import React, {ReactElement} from "react";
 import {useRouter} from "next/router";
 import NormalHead from "../../components/NormalHead";
 
-function AnimatePage(): ReactElement {
+export default function AnimatePage(): ReactElement {
   const router = useRouter();
   const {text} = router.query;
   return (
-      <div className={general.pageContainer}>
-        <NormalHead title={"" + text} />
-        <div className={styles.textContainer}>
-          <p className={styles.spinnyText}>
-            <span className={styles.rainbow}>{text}</span>
-          </p>
-        </div>
-        <div className={styles.textContainer}>
-          <p className={styles.spinnyText2}>
-            <span className={styles.rainbow2}>{text}</span>
-          </p>
-        </div>
+    <div className={general.pageContainer}>
+      <NormalHead title={"" + text} />
+      <div className={styles.textContainer}>
+        <p className={styles.spinnyText}>
+          <span className={styles.rainbow}>{text}</span>
+        </p>
       </div>
+      <div className={styles.textContainer}>
+        <p className={styles.spinnyText2}>
+          <span className={styles.rainbow2}>{text}</span>
+        </p>
+      </div>
+    </div>
   );
 }
 
-export default AnimatePage
+export async function getServerSideProps() {
+  return {props: {}};
+}
 
